e53fee06372761cb2f19b58d9590433b1d184d6c
Wayne Pacileo <wsfpacileo@gmail.com>
2020-02-09 21:50:22 -0500

    Assingment 2 for Web Mapping Class
    Used MNR Engines as date to create the page

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// when engine photo is clicked, run code
+$('.engineimages').click(function() {
+
+  // pull photo url from class attributes
+  var photoURL = $(this).attr('src')
+  // use photourl to display engine photo in #bigphoto
+  $('#bigphoto').css('background-image', `url(${photoURL})`)
+
+  // pull title from class attributes
+  var title = $(this).attr('title')
+  // display title in #enginename
+  $('#enginename').html(`${title}`)
+})
